1bf66cb0b93f0cbefbe527901634d14f078338c4
Aloysius <dr.aloysius@gmail.com>
2020-12-21 11:47:27 +0000

    initial parallel execution flow with callbacks

--- runConcurrent.js
+++ runConcurrent.js
@@ -1,6 +1,9 @@
 /**
  * Concurrently runs the functions in <collection>.
  * 
+ * * @param {Array} collection A list of functions to run
+ * @param {function({ taskIndex: number })} callback A function to run after each function in <collection>
+ * @param {function} finalCallback The function to run after the last function in <collection> has run
  */
 module.exports = function runConcurrent(collection, callback, finalCallback) {
   let completedTasks = 0;
