9b68a76ed62fa1d2997968a5fcc2eeea1fc6ad6b
Aloysius <dr.aloysius@gmail.com>
2020-12-24 00:51:48 +0000

    Keep track of error and results for each task

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const runConcurrent = require('./runConcurrent')
 const tasks = [
-  // 1. Each async task should accept a callback
-  (done) => setTimeout(done, 500),
-  (done) => setTimeout(done, 500),
-  (done) => setTimeout(done, 500),
+  // 1. Each async task should accept a callback called 'done'
+  (done) => {
+    setTimeout(
+      () => done(null, 1, 2),
+      1000)
+  },
+  (done) => {
+    setTimeout(
+      () => done(null, { msg: 'hi'}),
+      1500)
+  },
+  (done) => {
+    setTimeout(
+      () => done(new Error('My error here')),
+      1500)
+  },
+  (done) => {
+    setTimeout(
+      () => done(null, 'foo', 'baz'),
+      500)
+  },
 ];
 
 console.log('Let\'s go:')
 
 runConcurrent(
   tasks,
-  ({ taskIndex }) => {
-    console.log(`Ran ${taskIndex}`);
+  // Task done callback
+  ({ taskIndex, results, error }) => {
+    console.log(
+      `Ran task ${taskIndex} |`,
+      results.length ? 'results:' : '', results.length ? results : '',
+      error ? `${error.message}` : ''
+    )
   },
+  // All tasks done callback
   () => {
-    console.log('done !')
+    console.log('all tasks done !')
   }
 )
--- runConcurrent.js
+++ runConcurrent.js
@@ -1,7 +1,7 @@
 /**
  * Concurrently runs the functions in <collection>.
  * 
- * @param {Array< (done)=>{} >} collection A list of functions to run. Each function should have a callback parameter.
+ * @param {Array< (done)=>{} >} collection A list of tasks/functions to run. Each task should have a callback parameter called 'done' in the form (error, data1, data2, ...) => {}
  * @param {function({ taskIndex: number })} taskDoneCb A function to run after each function in <collection>
  * @param {function} allTasksDoneCb The function to run after the last function in <collection> has run
  */
@@ -9,8 +9,10 @@ module.exports = function runConcurrent(collection, taskDoneCb, allTasksDoneCb) 
   let completedTasks = 0;
   
   for (let i = 0, task; task = collection[i]; i++) {
-    task(() => {
-      taskDoneCb({ taskIndex: i });
+    // Implement the 'done' callback for a task
+    task((error, ...val) => {
+      const results = val
+      taskDoneCb({ taskIndex: i, results, error });
       if (++completedTasks === collection.length) {
         allTasksDoneCb()
       }
