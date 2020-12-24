344c2477f22ca7b38a327dfdb108d2ec971d05ac
Aloysius <dr.aloysius@gmail.com>
2020-12-24 02:45:38 +0000

    Add docs for runConcurrentLimit

--- runConcurrent.js
+++ runConcurrent.js
@@ -20,6 +20,15 @@ function runConcurrent(collection, taskDoneCb, allTasksDoneCb) {
   }
 }
 
+/**
+ * Concurrently runs the functions in <collection> with a concurrency limit
+ * 
+ * @param {Array< (done)=>{} >} collection A list of tasks/functions to run. Each task should have a callback parameter called 'done' in the form (error, data1, data2, ...) => {}
+ * @param {number} concurrencyLimit
+ * @param {function({ taskIndex: number })} taskDoneCb A function to run after each function in <collection>
+ * @param {function} allTasksDoneCb The function to run after the last function in <collection> has run
+ *
+ */
 function runConcurrentLimit(collection, concurrencyLimit, taskDoneCb, allTasksDoneCb) {
   if (collection.length === 0) {
     return allTasksDoneCb();
